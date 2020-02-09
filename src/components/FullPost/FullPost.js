06c340bb540273f9630066d51a41924e058025cb
OTree <kevin10410@gmail.com>
2020-02-09 21:57:09 +0800

    refactor(FullPost): render full post content by updatePostContent

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import { getPost } from '../../api/postsService';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        postContent: null,
+    }
 
-        );
+    isNeedToFetchPost = (selectedPostId) => {
+        return selectedPostId !== null
+            && (this.state.postContent === null
+            || selectedPostId !== this.state.postContent.id);
+    }
+
+    updatePostContent = async (postId) => {
+        const postContent = await getPost(postId)
+            .then(res => res.data)
+            .catch(() => null);
+
+        this.setState({ postContent });        
+    }
+
+    componentDidUpdate() {
+        const { selectedPostId } = this.props;
+
+        this.isNeedToFetchPost(selectedPostId)
+            && this.updatePostContent(selectedPostId);
+    }
+
+     render () {
+        const post = this.state.postContent === null
+            ? <p
+                style = {{ textAlign: "center" }}
+                >Please select a Post!</p>
+            : (
+                <div className="FullPost">
+                    <h1>{ this.state.postContent.title }</h1>
+                    <p>{ this.state.postContent.body }</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+            );
         return post;
     }
 }
