1219727e5156c71a69a5bbeb410f74023c8b78a9
OTree <kevin10410@gmail.com>
2020-02-09 21:53:37 +0800

    refactor(Blog): render posts by fetching all post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,14 +5,38 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+import { getAllPosts } from '../../api/postsService';
+
 class Blog extends Component {
+    state = {
+        allPosts: [],
+        selectedPostId: null,
+    }
+
+    componentDidMount = async () => {
+        const allPosts = await getAllPosts()
+            .then(res => res.data)
+            .then(data => data.filter(post => post.id < 5))
+            .catch(() => []);
+        
+        this.setState({ allPosts });
+    }
+
     render () {
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                {
+                    this.state.allPosts.map(post => (
+                        <Post
+                            key = { post.id }
+                            title = { post.title }
+                            author = 'OTree'
+                            postId = { post.id }
+                            updateSelectedPost = { this.handleUpdateSelectedPost }
+                        />
+                    ))
+                }
                 </section>
                 <section>
                     <FullPost />
