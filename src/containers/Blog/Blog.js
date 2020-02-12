a625cca86e4d5ac87c929b7983a2fe3669fe0264
OTree <kevin10410@gmail.com>
2020-02-12 21:06:51 +0800

    refactor(Blog): extract to Posts component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,69 +1,23 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
+import Posts from '../Posts/Posts';
 import NewPost from '../../components/NewPost/NewPost';
 import Header from '../../components/Header/Header';
 import './Blog.css';
 
-import { getAllPosts, deletePost } from '../../api/postsService';
-
 class Blog extends Component {
-    state = {
-        allPosts: [],
-        selectedPostId: null,
-    }
 
-    handleUpdateSelectedPost = (postId) => {
-        const selectedPostId = postId;
-        this.setState({ selectedPostId }); 
-    }
-
-    deletePostHandler = async (postId) => {
-        await deletePost(postId)
-            .then(res => res.data)
-            .then(data => { console.log(data); })
-            .catch(err => { console.log(err); });
-    }
-
-    componentDidMount = async () => {
-        const allPosts = await getAllPosts()
-            .then(res => res.data)
-            .then(data => data.filter(post => post.id < 5))
-            .catch(() => []);
-        
-        this.setState({ allPosts });
-    }
-
-    render () {
-        return (
-            <div>
-                <Header/>
-                <section className="Posts">
-                {
-                    this.state.allPosts.map(post => (
-                        <Post
-                            key = { post.id }
-                            title = { post.title }
-                            author = 'OTree'
-                            postId = { post.id }
-                            updateSelectedPost = { this.handleUpdateSelectedPost }
-                        />
-                    ))
-                }
-                </section>
-                <section>
-                    <FullPost
-                        deletePost = { this.deletePostHandler }
-                        selectedPostId = { this.state.selectedPostId }
-                    />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <Header />
+        <Posts />
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import { getAllPosts, deletePost } from '../../api/postsService';
+import './Posts.css';
+
+class Posts extends Component {
+  state = {
+    allPosts: [],
+    selectedPostId: null,
+  }
+
+  handleUpdateSelectedPost = (postId) => {
+    const selectedPostId = postId;
+    this.setState({ selectedPostId }); 
+  }
+
+  deletePostHandler = async (postId) => {
+    await deletePost(postId)
+      .then(res => res.data)
+      .then(data => { console.log(data); })
+      .catch(err => { console.log(err); });
+  }
+
+  componentDidMount = async () => {
+    const allPosts = await getAllPosts()
+      .then(res => res.data)
+      .then(data => data.filter(post => post.id < 5))
+      .catch(() => []);
+    
+    this.setState({ allPosts });
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <section className="Posts">
+          {
+            this.state.allPosts.map(post => (
+              <Post
+                key = { post.id }
+                title = { post.title }
+                author = 'OTree'
+                postId = { post.id }
+                updateSelectedPost = { this.handleUpdateSelectedPost }
+              />
+            ))
+          }
+        </section>
+        <section>
+          <FullPost
+            deletePost = { this.deletePostHandler }
+            selectedPostId = { this.state.selectedPostId }
+          />
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Posts;
